091184216bd738a102117a4519f5147b92277a96
Rhafu <rhafu29@gmail.com>
2024-01-06 11:17:24 -0300

    Global Scope and query with seasons relationship for series

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,14 +11,16 @@ class Serie extends Model
     use HasFactory;
 
     protected $fillable = ['nome'];
+    protected $with = ['seasons'];
 
     public function seasons()
     {
         return $this->hasMany(Season::class, 'series_id');
     }
 
-    public function episodes()
-    {
-        return $this->hasMany(Episode::class);
+    protected static function booted(){
+        self::addGlobalScope('ordered', function (Builder $queryBuilder) {
+            $queryBuilder->orderBy('nome');
+        });
     }
 }
