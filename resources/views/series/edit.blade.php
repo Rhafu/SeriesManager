bd4d8d6734c3d1f21c3edb037ced783435391937
Rhafu <rhafu29@gmail.com>
2024-01-06 11:38:42 -0300

    Editing edit form

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,15 @@
-<x-layout title="Editar Série '{{ $series->name }}'">
-  <x-form action="/series/{{ $series->id }}" name="{{ $series->name }}" :update="true"/>
+<x-layout title="Editar Série '{!! $series->name !!}'">
+  <form action="/series/{{ $series->id }}" method='post'>
+    @csrf
+    @method('PUT')
+    <div class="mb-3">
+      <label for="nome" class="form-label">Série: </label>
+      <input 
+        type="text"
+        name="name"
+        class="form-control"
+        @isset($series->name)value="{{$series->name}}"@endisset>
+      <button type="submit" class="btn btn-primary mt-2">Salvar</button>
+    </div>
+  </form>
 </x-layout>
